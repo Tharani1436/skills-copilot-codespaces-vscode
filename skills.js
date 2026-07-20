bf656a3e1602765b8972d2a8479dae842047c17c
THARANINATHAN O S <tharan.os.iot.2023@snsce.ac.in>
2026-07-20 18:12:56 +0000

    Complete comment suggestion step

--- skills.js
+++ skills.js
@@ -1,3 +1,6 @@
 function calculateNumbers(var1, var2) {
-    return var1 + var2;
+    // function to convert Celsius to Fahrenheit
+    var fahrenheit = (var1 * 9/5) + 32;
+    
+    return fahrenheit;
 }
